861ae75c45c0a6fa910d71689959a5009f52dfe0
SkywakerSLU <ffoe238@gmail.com>
2025-05-04 01:50:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,4 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
         });
     });
-
-    // Animación en el menú
-    const menuItems = document.querySelectorAll("#menu-lateral ul li a");
-    menuItems.forEach(item => {
-        item.addEventListener("mouseover", () => {
-            item.style.transform = "scale(1.1)";
-        });
-        item.addEventListener("mouseout", () => {
-            item.style.transform = "scale(1)";
-        });
-    });
 });
